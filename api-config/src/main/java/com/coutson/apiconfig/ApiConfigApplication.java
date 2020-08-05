befee861268af9c6e0214d38ba65a6d30d99dd32
unknown <wangrf@huaxindata.com.cn>
2020-08-05 16:08:40 +0800

    config

---
+++ api-config/src/main/java/com/coutson/apiconfig/ApiConfigApplication.java
@@ -0,0 +1,19 @@
+package com.coutson.apiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+@EnableEurekaServer
+public class ApiConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConfigApplication.class, args);
+    }
+
+}
---
+++ api-config/src/test/java/com/coutson/apiconfig/ApiConfigApplicationTests.java
@@ -0,0 +1,14 @@
+package com.coutson.apiconfig;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
